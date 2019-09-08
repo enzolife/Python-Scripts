03c10219917499bb19a92ca054a5a99935579369
Enzo.kuang <enzolife@foxmail.com>
2019-09-08 11:41:01 +0800

    20190908 change

--- Scripts/shopee_add_and_cancel_fans_new.py
+++ Scripts/shopee_add_and_cancel_fans_new.py
@@ -1,7 +1,8 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[7]:
+
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -14,6 +15,23 @@ import os
 
 # In[9]:
 
+
+# 获取脚本的当前路径，避免计划执行时路径出错
+home_dir = os.path.dirname(os.path.realpath(__file__))
+# 更换workding directory
+working_directory = home_dir
+os.chdir(working_directory)
+
+
+# In[82]:
+
+
+# pip install selenium
+
+
+# In[83]:
+
+
 # Date
 today_date = datetime.date.today() + datetime.timedelta(days=0)
 yesterday_date = datetime.date.today() + datetime.timedelta(days=-1)
@@ -22,12 +40,14 @@ seven_days_before_date = datetime.date.today() + datetime.timedelta(days=-7)
 today_date_string = today_date.strftime('%Y_%m_%d')
 
 
-# In[10]:
+# In[84]:
+
 
 # os.getcwd()
 
 
-# In[11]:
+# In[85]:
+
 
 log_file_name = 'shopee_add_and_cancel_fans_log\\shopee_add_and_cancel_fans_log_' + today_date_string + '.txt'
 
@@ -36,7 +56,8 @@ log_file_name = 'shopee_add_and_cancel_fans_log\\shopee_add_and_cancel_fans_log_
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-# In[12]:
+# In[86]:
+
 
 # 屏幕最大化，且指定下载目录
 options = webdriver.ChromeOptions()
@@ -47,49 +68,57 @@ prefs = {"profile.default_content_settings.popups": 0,
 options.add_experimental_option("prefs", prefs)
 
 
-# In[13]:
+# In[87]:
+
 
 # 使用chromedriver才可以用开发者权限
-chrome_driver_path = "D://Program Files (x86)//百度云同步盘//我的软件//chromedriver.exe"
+chrome_driver_path = ".//chrome_driver//chromedriver.exe"
 # browser = webdriver.Chrome(chrome_driver_path, chrome_options=options)
 
 
-# In[14]:
+# In[88]:
+
 
 # 所需参数
 # 站点；站点后缀；账户；密码；站点top卖家shopid；top卖家username（方便识别）
+# shop_list = [['tw', 'tw', 23070969, 'poweradapter.tw', 'kuangyiqiao1991', 9469128, 'alonso.tw'],
+#              ['tw', 'tw', 25482220, 'tengus.tw', 'tengus1803', 23068230, 'iu.tw'],
+#              ['tw', 'tw', 58707738, 'qianjiaozi', 'qianjiaozi888', 26513957, 'beibei.tw'],
+#              ['tw', 'tw', 66377809, 'qianjiaozi1', 'qianjiaozi888', 23068230, 'iu.tw'],
+#              ['tw', 'tw', 62416366, 'tengus1.tw', 'tengus1803', 23068230, 'iu.tw'],
+#              ['tw', 'tw', 62416544, 'tengus2.tw', 'tengus1803', 23068230, 'iu.tw'],
+#              ['tw', 'tw', 63534861, 'qianjiaozitw1', 'tengus1803', 23068230, 'iu.tw'],
+#              ['tw', 'tw', 62887142, 'yilanlu.tw', 'yilanlu888', 23481977, 'huoyi.tw'],
+#              ['my', 'com.my', 53580963, 'qianjiaozi.my', 'tengus1803', 10891137, 'winners.my'],
+#              ['my', 'com.my', 59848325, 'tengus.my', 'tengus1803', 10891137, 'winners.my'],
+#              ['my', 'com.my', 62418141, 'tengus1.my', 'tengus1803', 10891137, 'winners.my'],
+#              ['my', 'com.my', 62418493, 'tengus2.my', 'tengus1803', 10891137, 'winners.my'],
+#              ['id', 'co.id', 59846508, 'tengus.id', 'tengus1803', 28135012, 'sunnyfun.id'],
+#              ['id', 'co.id', 62417386, 'tengus1.id', 'tengus1803', 28135012, 'sunnyfun.id'],
+#              ['id', 'co.id', 62417551, 'tengus2.id', 'tengus1803', 28135012, 'sunnyfun.id']]
+
 shop_list = [['tw', 'tw', 23070969, 'poweradapter.tw', 'kuangyiqiao1991', 9469128, 'alonso.tw'],
-             ['tw', 'tw', 25482220, 'tengus.tw', 'tengus1803', 23068230, 'iu.tw'],
-             ['tw', 'tw', 58707738, 'qianjiaozi', 'qianjiaozi888', 26513957, 'beibei.tw'],
-             ['tw', 'tw', 66377809, 'qianjiaozi1', 'qianjiaozi888', 23068230, 'iu.tw'],
-             ['tw', 'tw', 62416366, 'tengus1.tw', 'tengus1803', 23068230, 'iu.tw'],
-             ['tw', 'tw', 62416544, 'tengus2.tw', 'tengus1803', 23068230, 'iu.tw'],
-             ['tw', 'tw', 63534861, 'qianjiaozitw1', 'tengus1803', 23068230, 'iu.tw'],
-             ['tw', 'tw', 62887142, 'yilanlu.tw', 'yilanlu888', 23481977, 'huoyi.tw'],
-             ['my', 'com.my', 53580963, 'qianjiaozi.my', 'tengus1803', 10891137, 'winners.my'],
-             ['my', 'com.my', 59848325, 'tengus.my', 'tengus1803', 10891137, 'winners.my'],
-             ['my', 'com.my', 62418141, 'tengus1.my', 'tengus1803', 10891137, 'winners.my'],
-             ['my', 'com.my', 62418493, 'tengus2.my', 'tengus1803', 10891137, 'winners.my'],
-             ['id', 'co.id', 59846508, 'tengus.id', 'tengus1803', 28135012, 'sunnyfun.id'],
-             ['id', 'co.id', 62417386, 'tengus1.id', 'tengus1803', 28135012, 'sunnyfun.id'],
-             ['id', 'co.id', 62417551, 'tengus2.id', 'tengus1803', 28135012, 'sunnyfun.id']]
+             ['th', 'co.th', 117213614, 'tengus.th', 'tengus1803', 25926687, 'xiaozhainv']]
 
 
-# In[15]:
+# In[89]:
+
 
 # 转换为dataframe
 shop_df_columns = ['site', 'site_suffix', 'shopid', 'acc', 'pwd', 'top_shop_id', 'top_shop_username']
 shop_df = pd.DataFrame(shop_list, columns=shop_df_columns)
 
 
-# In[16]:
+# In[90]:
+
 
 # 关注及关注中多语言
-following_language = ['关注中', '關注中', 'Following', 'Mengikuti']
-not_following_language = ['+ 关注', '+ 關注', '+ Follow', '+ Ikuti']
+following_language = ['关注中', '關注中', 'Following', 'Mengikuti', 'กำลังติดตาม']
+not_following_language = ['+ 关注', '+ 關注', '+ Follow', '+ Ikuti', "+ ติดตาม"]
 
 
-# In[17]:
+# In[91]:
+
 
 # 取关后关注函数
 def add_and_cancel_fans(site, site_suffix, shopid, acc, pwd, top_shop_id, top_shop_username):
@@ -103,7 +132,9 @@ def add_and_cancel_fans(site, site_suffix, shopid, acc, pwd, top_shop_id, top_sh
     time.sleep(10)
     
     # remove ads, refresh again
-    browser.get(main_page_url)
+    browser.refresh()
+    time.sleep(10)
+    browser.refresh()
     time.sleep(10)
 
     # if the shop is on my site, should press the button to choose language
@@ -122,7 +153,7 @@ def add_and_cancel_fans(site, site_suffix, shopid, acc, pwd, top_shop_id, top_sh
     time.sleep(10)
     
     # 输入
-    acc_password_input_elem = browser.find_elements_by_css_selector('.input-with-status__input')
+    acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
     try:
         acc_password_input_elem[0].click()
         acc_password_input_elem[0].send_keys(acc)
@@ -130,7 +161,7 @@ def add_and_cancel_fans(site, site_suffix, shopid, acc, pwd, top_shop_id, top_sh
         acc_password_input_elem[1].send_keys(pwd)
     except:
         time.sleep(30)
-        acc_password_input_elem = browser.find_elements_by_css_selector('.input-with-status__input')
+        acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
         acc_password_input_elem[0].click()
         acc_password_input_elem[0].send_keys(acc)
         acc_password_input_elem[1].click()
@@ -138,7 +169,7 @@ def add_and_cancel_fans(site, site_suffix, shopid, acc, pwd, top_shop_id, top_sh
     time.sleep(10)
     
     # 点击提交
-    Login_button_elem = browser.find_elements_by_css_selector('.shopee-button-solid.shopee-button-solid--primary')
+    Login_button_elem = browser.find_elements_by_css_selector('._2DvX7K._3j9-lD._3ddytl.SjORHu')
     Login_button_elem[0].click()
     logging.info('Login completed.')
     time.sleep(10)
@@ -231,7 +262,8 @@ def add_and_cancel_fans(site, site_suffix, shopid, acc, pwd, top_shop_id, top_sh
     browser.quit()
 
 
-# In[18]:
+# In[92]:
+
 
 # 历遍所有shop
 for index, my_shop in shop_df.iterrows():
@@ -252,23 +284,27 @@ for index, my_shop in shop_df.iterrows():
         logging.info('An exception occurred: ' + str(err) + '.')
 
 
-# In[19]:
+# In[93]:
+
 
 # browser = webdriver.Chrome(chrome_driver_path, chrome_options=options)
 
 
-# In[20]:
+# In[94]:
+
 
 # browser.get('https://shopee.com.my')
 
 
-# In[21]:
+# In[95]:
+
 
 # language_selector = browser.find_elements_by_css_selector('.shopee-button-outline.shopee-button-outline--primary-reverse')
 # len(language_selector)
 
 
-# In[22]:
+# In[96]:
+
 
 #language_selector[2].click()
 

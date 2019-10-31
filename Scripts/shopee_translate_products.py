904e819e7dd9c561e4da733d8f4f0bd39dcc0caa
Enzo.kuang <enzolife@foxmail.com>
2019-10-31 22:51:34 +0800

    20191031 change(1)

--- Scripts/shopee_translate_products.py
+++ Scripts/shopee_translate_products.py
@@ -1,7 +1,8 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[3]:
+
 
 import pandas as pd
 import os
@@ -10,7 +11,8 @@ import logging
 from datetime import date
 
 
-# In[13]:
+# In[4]:
+
 
 # 获取脚本的当前路径，避免计划执行时路径出错
 home_dir = os.path.dirname(os.path.realpath(__file__))
@@ -19,12 +21,14 @@ working_directory = home_dir
 os.chdir(working_directory)
 
 
-# In[14]:
+# In[5]:
+
 
 logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 
-# In[15]:
+# In[6]:
+
 
 folder_path = '../shopee_product_translation/'
 tw_site_product_list = pd.read_excel(os.path.join(folder_path, 'tengus_tw_product_list.xlsx'))
@@ -33,29 +37,38 @@ tw_site_product_list = pd.read_excel(os.path.join(folder_path, 'tengus_tw_produc
 # tw_site_product_list.head(5)
 
 
-# In[16]:
+# In[7]:
+
 
 # tw_site_product_list.columns
 
 
-# In[17]:
-
-# tw_site_product_list = tw_site_product_list[tw_site_product_list['产品id'] == 2496637042]
+# In[8]:
 
 
-# In[18]:
+# tw_site# 获取脚本的当前路径，避免计划执行时路径出错
+# home_dir = os.path.dirname(os.path.realpath(__file__))
+# # 更换workding directory
+# working_directory = home_dir
+# os.chdir(working_directory)_product_list = tw_site_product_list[tw_site_product_list['产品id'] == 2496637042]
+
+
+# In[9]:
+
 
 # top_cat_in_tw = tw_site_product_list[['分类ID', '产品id']].groupby('分类ID').agg({'产品id': 'nunique'}).reset_index().sort_values(by='产品id', ascending=False).reset_index().head(5)
 # top_cat_in_tw
 
 
-# In[19]:
+# In[10]:
+
 
 # top_cat_id = top_cat_in_tw['分类ID'][0]
 # top_cat_id
 
 
-# In[20]:
+# In[11]:
+
 
 output_excel_columns = [
                         '变种名称',
@@ -85,7 +98,8 @@ output_excel_columns = [
 ]
 
 
-# In[21]:
+# In[12]:
+
 
 tw_site_product_list_selected_columns = ['分类ID', '产品属性', 'Parent SKU', '产品标题', '产品描述', 'sku', '变种名称', '变种属性名称一',
        '变种属性名称二', '变种属性值一', '变种属性值二', '价格', '库存', '重量', '主图（URL）地址', '附图1',
@@ -93,26 +107,30 @@ tw_site_product_list_selected_columns = ['分类ID', '产品属性', 'Parent SKU
        '宽（cm）', '高（cm）', '发货期', '来源URL', '尺码图']
 
 
-# In[22]:
+# In[13]:
+
 
 tw_site_product_list_with_selected_columns = tw_site_product_list[tw_site_product_list_selected_columns]
 # tw_site_product_list_with_selected_columns.head(5)
 
 
-# In[23]:
+# In[14]:
+
 
 # tw_site_product_list_with_selected_columns = tw_site_product_list_with_selected_columns[tw_site_product_list_with_selected_columns['分类ID'] == top_cat_id].reset_index()
 # tw_site_product_list_with_selected_columns
 
 
-# In[24]:
+# In[15]:
+
 
 # 把1688的来源url拆出来，把productid填充为parent sku
 tw_site_product_list_with_selected_columns['Parent SKU'] = tw_site_product_list_with_selected_columns['来源URL'].str.split("/", n = 4, expand = True)[4].str.split(".", n = 1, expand=True)[0]
 # tw_site_product_list_with_selected_columns.head(5)
 
 
-# In[25]:
+# In[16]:
+
 
 # remove emoji
 import re
@@ -139,13 +157,15 @@ def remove_emoji(text):
 # remove_emoji('ins爆款 🥕胡蘿卜🥕寶寶針織竹節棉爬服 嬰兒兔裝 嬰兒連身衣 寶寶包屁衣連身衣 中間開襟包屁衣 寶寶連體衣哈衣爬服')
 
 
-# In[26]:
+# In[17]:
+
 
 tw_site_product_list_with_selected_columns['产品标题'] = [remove_emoji(x) for x in tw_site_product_list_with_selected_columns['产品标题']]
 tw_site_product_list_with_selected_columns['产品描述'] = [remove_emoji(x) for x in tw_site_product_list_with_selected_columns['产品描述']]
 
 
-# In[27]:
+# In[18]:
+
 
 tw_site_product_list_with_selected_columns['长（cm）'] = tw_site_product_list_with_selected_columns['长（cm）'].fillna(18)
 tw_site_product_list_with_selected_columns['宽（cm）'] = tw_site_product_list_with_selected_columns['宽（cm）'].fillna(13)
@@ -153,24 +173,28 @@ tw_site_product_list_with_selected_columns['高（cm）'] = tw_site_product_list
 tw_site_product_list_with_selected_columns['发货期'] = tw_site_product_list_with_selected_columns['发货期'].fillna(2)
 
 
-# In[28]:
+# In[19]:
+
 
 translate_currency = 458.61
 profit_rate = 1.2
 tw_site_product_list_with_selected_columns['价格'] = tw_site_product_list_with_selected_columns['价格'] * translate_currency / profit_rate
 
 
-# In[29]:
+# In[20]:
+
 
 # tw_site_product_list_with_selected_columns.head(5)
 
 
-# In[30]:
+# In[21]:
+
 
 from googletrans import Translator
 
 
-# In[31]:
+# In[22]:
+
 
 translator = Translator(service_urls=[
       'translate.google.com',
@@ -179,60 +203,69 @@ translator = Translator(service_urls=[
 # translator.translate('韩国', dest='ko').text
 
 
-# In[32]:
+# In[23]:
+
 
 # translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ko')
 # for translation in translations:
 #     print(translation.origin, ' -> ', translation.text)
 
 
-# In[33]:
+# In[24]:
+
 
 translate_columns = ['产品标题', '产品描述', '变种名称', '变种属性名称一', '变种属性名称二', '变种属性值一', '变种属性值二']
 # translate_columns = ['产品标题']
 # translate_columns = ['产品描述', '变种名称', '变种属性名称一', '变种属性名称二', '变种属性值一', '变种属性值二']
 
 
-# In[34]:
+# In[25]:
+
 
 # 把空值填充为string格式
 tw_site_product_list_with_selected_columns[translate_columns] = tw_site_product_list_with_selected_columns[translate_columns].fillna(value='不需翻译')
 
 
-# In[35]:
+# In[26]:
+
 
 # 把variation_name做一些删减
 variation_list = set(list(tw_site_product_list_with_selected_columns['变种属性名称一'].unique()) + list(tw_site_product_list_with_selected_columns['变种属性名称二'].unique()))
 logging.info('variation list: ' + str(variation_list))
 
 
-# In[36]:
+# In[27]:
+
 
 variation_rename = {'适合身高': '身高', '童袜尺码': '尺码', '不需翻译': 'not set'}
 tw_site_product_list_with_selected_columns['变种属性名称一'] = tw_site_product_list_with_selected_columns['变种属性名称一'].apply(lambda x: variation_rename[x] if x in variation_rename else x)
 tw_site_product_list_with_selected_columns['变种属性名称二'] = tw_site_product_list_with_selected_columns['变种属性名称二'].apply(lambda x: variation_rename[x] if x in variation_rename else x)
 
 
-# In[37]:
+# In[28]:
+
 
 # 对variation_value做一些删减
 # variation_value_list = set(list(tw_site_product_list_with_selected_columns['变种属性值一'].unique()) + list(tw_site_product_list_with_selected_columns['变种属性值一'].unique()))
 # logging.info('variation value list: ' + str(variation_value_list))
 
 
-# In[38]:
+# In[29]:
 
-tw_site_product_list_with_selected_columns = tw_site_product_list_with_selected_columns[:5]
+
+# tw_site_product_list_with_selected_columns = tw_site_product_list_with_selected_columns[:5]
 # # tw_site_product_list_with_selected_columns
 
 
-# In[39]:
+# In[30]:
+
 
 today = date.today()
 d1 = today.strftime("%Y_%m_%d")
 
 
-# In[40]:
+# In[31]:
+
 
 # for index, row in tw_site_product_list_with_selected_columns.iterrows():
 #     try:
@@ -260,7 +293,8 @@ d1 = today.strftime("%Y_%m_%d")
 #         pass
 
 
-# In[41]:
+# In[ ]:
+
 
 for index, row in tw_site_product_list_with_selected_columns.iterrows():
     translated = 0
@@ -291,7 +325,8 @@ for index, row in tw_site_product_list_with_selected_columns.iterrows():
             pass        
 
 
-# In[42]:
+# In[ ]:
+
 
 # for translate_column in translate_columns:
 #     # tw_site_product_list_with_selected_columns[translate_column] = tw_site_product_list_with_selected_columns[translate_column].fillna(0)

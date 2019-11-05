dcfbc704d0db843bff21344b4b21ae6cc43ac1d8
Enzo.kuang <enzolife@foxmail.com>
2019-11-05 23:02:57 +0800

    20191105 change

--- Scripts/shopee_add_fans_to_maximum.py
+++ Scripts/shopee_add_fans_to_maximum.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[53]:
+# In[78]:
 
 
 from selenium import webdriver
@@ -13,7 +13,7 @@ import os
 import datetime
 
 
-# In[54]:
+# In[79]:
 
 
 # 获取脚本的当前路径，避免计划执行时路径出错
@@ -23,13 +23,13 @@ working_directory = home_dir
 os.chdir(working_directory)
 
 
-# In[55]:
+# In[80]:
 
 
 logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
 
-# In[56]:
+# In[81]:
 
 
 # 屏幕最大化，且指定下载目录
@@ -42,95 +42,224 @@ prefs = {"profile.default_content_settings.popups": 0,
 options.add_experimental_option("prefs", prefs)
 
 
-# In[57]:
+# In[82]:
 
 
 # 使用chromedriver才可以用开发者权限
-chrome_driver_path = ".//chrome_driver//chromedriver.exe"
-browser = webdriver.Chrome(chrome_driver_path, chrome_options=options)
+chrome_driver_path = ".//chrome_driver//chromedriver.exe" # 获取脚本的当前路径，避免计划执行时路径出错
+# home_dir = os.path.dirname(os.path.realpath(__file__))
+# # 更换workding directory
+# working_directory = home_dir
+# os.chdir(working_directory)r//chromedriver.exe"
+# browser = webdriver.Chrome(chrome_driver_path, chrome_options=options)
 
 
-# In[58]:
+# In[83]:
 
 
 # main page
-main_page_url = "http://shopee.sg"
-browser.get(main_page_url)
+# main_page_url = "http://shopee.sg"
 
-# remove ads, refresh again
-for i in range(5):
-    browser.get(main_page_url)
+shop_info = [["https://shopee.co.id", 'tengus.id', 'tengus1803', 11184349, '+ Ikuti'],
+             ['https://shopee.sg', 'tengus1.sg', 'tengus1803', 11918, '+ Follow'],
+             ['https://shopee.ph', 'tengus.ph', 'tengus1803', 2215148, '+ Follow']]
+
+shop_info = [['https://shopee.sg', 'tengus1.sg', 'tengus1803', 11918, '+ Follow'],
+             ['https://shopee.ph', 'tengus.ph', 'tengus1803', 2215148, '+ Follow']]
+
+
+# main_page_url = "http://shopee.co.id"
+# browser.get(main_page_url)
+
+# # remove ads, refresh again
+# for i in range(5):
+#     browser.get(main_page_url)
+#     time.sleep(10)
+
+
+# In[84]:
+
+
+for shop in shop_info:
+    home_page = shop[0]
+    shop_name = shop[1]
+    shop_psw = shop[2]
+    top_shop_id = shop[3]
+    follow_button_text = shop[4]
+    
+    browser = webdriver.Chrome(chrome_driver_path, chrome_options=options)
+    browser.get(home_page)
+    
+    # remove ads, refresh again
+    for i in range(5):
+        browser.get(home_page)
+        time.sleep(10)
+        
+    # login
+    LoginElem = browser.find_elements_by_css_selector('.navbar__link.navbar__link--account.navbar__link--tappable.navbar__link--hoverable.navbar__link-text.navbar__link-text--medium')
+    '''
+    有关Selenium Compound class names not permitted的错误
+    可以参考https://stackoverflow.com/questions/37771604/selenium-compound-class-names-not-permitted
+    使用css_selector解决即可
+    '''
+    # 点击登入，弹出账户密码输入框
+    LoginElem[1].click()
     time.sleep(10)
-
-
-# In[59]:
-
-
-# login
-LoginElem = browser.find_elements_by_css_selector('.navbar__link.navbar__link--account.navbar__link--tappable.navbar__link--hoverable.navbar__link-text.navbar__link-text--medium')
-'''
-有关Selenium Compound class names not permitted的错误
-可以参考https://stackoverflow.com/questions/37771604/selenium-compound-class-names-not-permitted
-使用css_selector解决即可
-'''
-# 点击登入，弹出账户密码输入框
-LoginElem[1].click()
-time.sleep(10)
-
-
-# In[60]:
-
-
-acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
-try:
-    acc_password_input_elem[0].click()
-    acc_password_input_elem[0].send_keys('tengus1.sg')
-    acc_password_input_elem[1].click()
-    acc_password_input_elem[1].send_keys('tengus1803')
-except:
-    time.sleep(30)
+    
     acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
-    acc_password_input_elem[0].click()
-    acc_password_input_elem[0].send_keys('tengus1.sg')
-    acc_password_input_elem[1].click()
-    acc_password_input_elem[1].send_keys('tengus1803')
-time.sleep(10)
+    try:
+        #     acc_password_input_elem[0].click()
+        #     acc_password_input_elem[0].send_keys('tengus1.sg')
+        #     acc_password_input_elem[1].click()
+        #     acc_password_input_elem[1].send_keys('tengus1803')
+
+        acc_password_input_elem[0].click()
+        acc_password_input_elem[0].send_keys(shop_name)
+        acc_password_input_elem[1].click()
+        acc_password_input_elem[1].send_keys(shop_psw)    
+    except:
+        time.sleep(30)
+        #     acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
+        #     acc_password_input_elem[0].click()
+        #     acc_password_input_elem[0].send_keys('tengus1.sg')
+        #     acc_password_input_elem[1].click()
+        #     acc_password_input_elem[1].send_keys('tengus1803')
+        acc_password_input_elem[0].click()
+        acc_password_input_elem[0].send_keys(shop_name)
+        acc_password_input_elem[1].click()
+        acc_password_input_elem[1].send_keys(shop_psw)  
+
+    time.sleep(10) 
+    
+    Login_button_elem = browser.find_elements_by_css_selector('._2DvX7K._3j9-lD._3ddytl.SjORHu')
+    time.sleep(10)
+    Login_button_elem[0].click()
+    time.sleep(10)    
+    
+    top_shop_url = home_page + '/shop/' + str(top_shop_id) + '/followers/?__classic__=1'
+    browser.get(top_shop_url)
+    
+    # add certain number of fans
+    to_add_num_of_following = 400
+    num_of_following_display = len(browser.find_elements_by_css_selector('.clickable_area.middle-centered-div'))
+    
+    following_buttons = browser.find_elements_by_css_selector('.btn-follow.follow.L14')
+    # len(following_buttons)   
+    
+    i = 0
+    total_add_num_of_following = to_add_num_of_following
+    
+    browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + Keys.HOME)
+    
+    while i <= total_add_num_of_following - 1:  
+        while to_add_num_of_following > 0:
+            if i + 1 > len(following_buttons):
+                browser.find_element_by_css_selector('body').send_keys(Keys.PAGE_DOWN)
+                time.sleep(10)
+
+            following_buttons = browser.find_elements_by_css_selector('.btn-follow.follow.L14')
+            logging.info('Now we have ' + str(len(following_buttons)) + ' following buttons on the screen.')
+            logging.info('Now run the ' + str(i + 1) + ' time.')
+            # if following_buttons[i].text == '+ 關注':
+            # if following_buttons[i].text == '+ ติดตาม':
+            # if following_buttons[i].text == '+ Follow':
+            if following_buttons[i].text == follow_button_text:
+                # print(following_buttons[i].text)
+                shopid = following_buttons[i].get_attribute('shopid')
+                following_buttons[i].click()
+                time.sleep(5)
+                to_add_num_of_following -= 1
+                logging.info(str(shopid) + ' is following now, ' + str(to_add_num_of_following) + ' following remains.')
+            else:
+                logging.warning('Skip this one. It\'s following already.')
+            i += 1    
+            
+    # 关闭
+    browser.quit()
 
 
-# In[61]:
+# In[85]:
 
 
-Login_button_elem = browser.find_elements_by_css_selector('._2DvX7K._3j9-lD._3ddytl.SjORHu')
-time.sleep(10)
-Login_button_elem[0].click()
-time.sleep(10)
+# # login
+# LoginElem = browser.find_elements_by_css_selector('.navbar__link.navbar__link--account.navbar__link--tappable.navbar__link--hoverable.navbar__link-text.navbar__link-text--medium')
+# '''
+# 有关Selenium Compound class names not permitted的错误
+# 可以参考https://stackoverflow.com/questions/37771604/selenium-compound-class-names-not-permitted
+# 使用css_selector解决即可
+# '''
+# # 点击登入，弹出账户密码输入框
+# LoginElem[1].click()
+# time.sleep(10)
 
 
-# In[62]:
+# In[86]:
+
+
+# acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
+# try:
+# #     acc_password_input_elem[0].click()
+# #     acc_password_input_elem[0].send_keys('tengus1.sg')
+# #     acc_password_input_elem[1].click()
+# #     acc_password_input_elem[1].send_keys('tengus1803')
+    
+#     acc_password_input_elem[0].click()
+#     acc_password_input_elem[0].send_keys('tengus.id')
+#     acc_password_input_elem[1].click()
+#     acc_password_input_elem[1].send_keys('tengus1803')    
+# except:
+#     time.sleep(30)
+# #     acc_password_input_elem = browser.find_elements_by_css_selector('._2QBp41._1b-IZR')
+# #     acc_password_input_elem[0].click()
+# #     acc_password_input_elem[0].send_keys('tengus1.sg')
+# #     acc_password_input_elem[1].click()
+# #     acc_password_input_elem[1].send_keys('tengus1803')
+
+#     acc_password_input_elem[0].click()
+#     acc_password_input_elem[0].send_keys('tengus.id')
+#     acc_password_input_elem[1].click()
+#     acc_password_input_elem[1].send_keys('tengus1803')  
+
+# time.sleep(10)
+
+
+# In[87]:
+
+
+# Login_button_elem = browser.find_elements_by_css_selector('._2DvX7K._3j9-lD._3ddytl.SjORHu')
+# time.sleep(10)
+# Login_button_elem[0].click()
+# time.sleep(10)
+
+
+# In[88]:
 
 
 # change current tab size
 # browser.set_window_size(400, 862)    
 
 
-# In[63]:
+# In[89]:
 
 
-# add fans from Top Seller's shop
-top_shop_id = 11918
-top_shop_url = 'https://shopee.sg/shop/' + str(top_shop_id) + '/followers/?__classic__=1'
-browser.get(top_shop_url)
+# # add fans from Top Seller's shop
+# # top_shop_id = 11918 # sg
+# top_shop_id = 145423 # id
+
+# # top_shop_url = 'https://shopee.sg/shop/' + str(top_shop_id) + '/followers/?__classic__=1'
+# top_shop_url = 'https://shopee.co.id/shop/' + str(top_shop_id) + '/followers/?__classic__=1'
+# browser.get(top_shop_url)
 
 
-# In[64]:
+# In[90]:
 
 
-# add certain number of fans
-to_add_num_of_following = 400
-num_of_following_display = len(browser.find_elements_by_css_selector('.clickable_area.middle-centered-div'))
+# # add certain number of fans
+# to_add_num_of_following = 400
+# num_of_following_display = len(browser.find_elements_by_css_selector('.clickable_area.middle-centered-div'))
 
 
-# In[65]:
+# In[91]:
 
 
 # page down until we get at least 400 fans to cancel
@@ -144,55 +273,56 @@ num_of_following_display = len(browser.find_elements_by_css_selector('.clickable
 # num_of_following_display
 
 
-# In[66]:
+# In[92]:
 
 
-following_buttons = browser.find_elements_by_css_selector('.btn-follow.follow.L14')
-# len(following_buttons)
+# following_buttons = browser.find_elements_by_css_selector('.btn-follow.follow.L14')
+# # len(following_buttons)
 
 
-# In[67]:
+# In[93]:
 
 
-i = 0
-total_add_num_of_following = to_add_num_of_following
+# i = 0
+# total_add_num_of_following = to_add_num_of_following
 
 
-# In[68]:
+# In[94]:
 
 
-browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + Keys.HOME)
+# browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + Keys.HOME)
 
 
-# In[69]:
+# In[95]:
 
 
-while i <= total_add_num_of_following - 1:  
-    while to_add_num_of_following > 0:
-        if i + 1 > len(following_buttons):
-            browser.find_element_by_css_selector('body').send_keys(Keys.PAGE_DOWN)
-            time.sleep(10)
+# while i <= total_add_num_of_following - 1:  
+#     while to_add_num_of_following > 0:
+#         if i + 1 > len(following_buttons):
+#             browser.find_element_by_css_selector('body').send_keys(Keys.PAGE_DOWN)
+#             time.sleep(10)
         
-        following_buttons = browser.find_elements_by_css_selector('.btn-follow.follow.L14')
-        logging.info('Now we have ' + str(len(following_buttons)) + ' following buttons on the screen.')
-        logging.info('Now run the ' + str(i + 1) + ' time.')
-        # if following_buttons[i].text == '+ 關注':
-        # if following_buttons[i].text == '+ ติดตาม':
-        if following_buttons[i].text == '+ Follow':
-            # print(following_buttons[i].text)
-            shopid = following_buttons[i].get_attribute('shopid')
-            following_buttons[i].click()
-            time.sleep(5)
-            to_add_num_of_following -= 1
-            logging.info(str(shopid) + ' is following now, ' + str(to_add_num_of_following) + ' following remains.')
-        else:
-            logging.warning('Skip this one. It\'s following already.')
-        i += 1
+#         following_buttons = browser.find_elements_by_css_selector('.btn-follow.follow.L14')
+#         logging.info('Now we have ' + str(len(following_buttons)) + ' following buttons on the screen.')
+#         logging.info('Now run the ' + str(i + 1) + ' time.')
+#         # if following_buttons[i].text == '+ 關注':
+#         # if following_buttons[i].text == '+ ติดตาม':
+#         # if following_buttons[i].text == '+ Follow':
+#         if following_buttons[i].text == '+ Ikuti':
+#             # print(following_buttons[i].text)
+#             shopid = following_buttons[i].get_attribute('shopid')
+#             following_buttons[i].click()
+#             time.sleep(5)
+#             to_add_num_of_following -= 1
+#             logging.info(str(shopid) + ' is following now, ' + str(to_add_num_of_following) + ' following remains.')
+#         else:
+#             logging.warning('Skip this one. It\'s following already.')
+#         i += 1
 
 
-# In[70]:
+# In[96]:
 
 
-# 关闭
-browser.quit()
+# # 关闭
+# browser.quit()
 
